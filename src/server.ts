f601c77fe6062793edf9689d3cf19f3d00e4f700
Abrorkhon <akon.xan@mail.ru>
2024-09-03 18:06:42 +0900

    Initial setup

---
+++ src/server.ts
@@ -0,0 +1,7 @@
+console.log("executed");
+const person: string = "martin";
+const count: number = 100;
+import moment from "moment";
+
+const currentTime = moment().format("YYYY MM DD")
+console.log(currentTime);
