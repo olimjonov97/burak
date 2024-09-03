70a394a92f11e982b3e30a653ed643402a82f4f2
Abrorkhon <akon.xan@mail.ru>
2024-09-03 19:09:41 +0900

    Task G

--- train.js
+++ train.js
@@ -1,19 +1,31 @@
 
-function getHighestIndex(myArray){
+// function getHighestIndex(myArray){
 
-    let maxValue =Math.max(...myArray)
-    console.log(maxValue)
+//     let maxValue =Math.max(...myArray)
+//     console.log(maxValue)
 
-    return myArray.indexOf(maxValue)
+//     return myArray.indexOf(maxValue)
 
+// }
+// console.log(getHighestIndex([5, 21, 12, 1, 8]));
+function getHighestIndex(myArray) {
+  let maxIndex = 0; 
+  
+  for (let i = 0; i < myArray.length; i++) {
+  
+    for (let j = 0; j < myArray.length; j++) {
+      console.log(
+        `Comparing myArray[${i}] = ${myArray[i]} with myArray[${j}] = ${myArray[j]}`
+      );
+  
+      if (myArray[i] > myArray[maxIndex]) {
+        maxIndex = i;
+      }
+    }
+  }
+
+  
+  return maxIndex;
 }
-console.log(getHighestIndex([5, 21, 12, 1, 8]));
 
-//  for (let i = 0; i < myArray.length; i++) {
-//    // console.log("I value =>", myArray[i]);
-//    for (let j = 0; j <= i; j++) {
-//      console.log("I value =>", myArray[i]);
-//      console.log("J value =>", myArray[j]);
-//    }
-//    console.log(">>>>>>>>>>>");
-//  }
+console.log(getHighestIndex([5, 21, 12, 101, 8]));
