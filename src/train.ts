68082711da9af27ac6ab3fc407bb6b8531dae462
Abrorkhon <akon.xan@mail.ru>
2024-09-05 18:16:00 +0900

    TASK H

--- src/train.ts
+++ src/train.ts
@@ -1,4 +1,4 @@
-
+//  TASK G
 // function getHighestIndex(myArray){
 
 //     let maxValue =Math.max(...myArray)
@@ -8,27 +8,37 @@
 
 // }
 // console.log(getHighestIndex([5, 21, 12, 1, 8]));
-function getHighestIndex(myArray:Number[]=[]) {
-  
-  let maxIndex:number=0;
+// function getHighestIndex(myArray:Number[]=[]) {
 
-  
-  for (let i = 0; i < myArray.length; i++) {
-    for (let j = 0; j < myArray.length; j++) {
-      console.log(
-        `Comparing myArray[${i}] = ${myArray[i]} with myArray[${j}] = ${myArray[j]}`
-      );
-  
-      if (myArray[i] > myArray[maxIndex]) {
-        maxIndex = i;
-      }
-    }
-  }
+//   let maxIndex:number=0;
 
-  
-  return maxIndex;
-}
+//   for (let i = 0; i < myArray.length; i++) {
+//     for (let j = 0; j < myArray.length; j++) {
+//       console.log(
+//         `Comparing myArray[${i}] = ${myArray[i]} with myArray[${j}] = ${myArray[j]}`
+//       );
 
-console.log(getHighestIndex([5, 21, 12, 101, 8]));
+//       if (myArray[i] > myArray[maxIndex]) {
+//         maxIndex = i;
+//       }
+//     }
+//   }
+
+//   return maxIndex;
+// }
+
+// console.log(getHighestIndex([5, 21, 12, 101, 8]));
 
 // console.log("Hello MIT");
+
+//TASK H
+function isPositive(value: number) {
+  return value > 0;
+}
+function getPositive(arr: number[]) {
+  return arr.filter(isPositive).join("");
+}
+console.log(getPositive([1, -4, 2]));
+console.log(getPositive)
+// getPositive([-5, 10, -2, 3]);
+// getPositive([0, -1, -2, 4, 5])
