0d22fa6566bd6bcae8768e812140f8eb356ec30c
Abrorkhon <akon.xan@mail.ru>
2024-09-05 17:28:58 +0900

    Feat Connection vie Moongoose

--- src/server.ts
+++ src/server.ts
@@ -8,17 +8,27 @@
 
 // Architectural pattern MVC(model view Controoller) DI(dependenci iinjection),,MVPmodel view presenter
 
-// Design Pattern Middle Ware, Decorator, 
- 
+// Design Pattern Middle Ware, Decorator,
+
 //En
 
 // import moment from "moment"; // const moment require("moment") in common js
 
-
-// Mongo Db 1 
+// Mongo Db 1
 // Cluster => DATABASE (Reja & Burak) Collecion (plans) => Document(data birligi)
-
+// console.log("PORT", process.env.PORT)
+// console.log("Mongo", process.env.MONGO_URL)
 import dotenv from "dotenv";
 dotenv.config();
-console.log("PORT", process.env.PORT)
-console.log("Mongo", process.env.MONGO_URL)
+
+import mongoose from "mongoose";
+
+mongoose
+  .connect(process.env.MONGO_URL as string, {})
+  .then((data) => {
+    console.log("Success On MongoDb connection");
+    const PORT = process.env.PORT ?? 3003;
+  })
+  .catch((err) => {
+    console.log("ERROR on MongoDb connection", err);
+  });
