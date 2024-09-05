cbc6b9b1950fce08640622e04540132e3f60eb72
Abrorkhon <akon.xan@mail.ru>
2024-09-05 17:46:50 +0900

    feat express integration

---
+++ src/app.ts
@@ -0,0 +1,20 @@
+import express from "express";
+import path from "path";
+// 1-Entrance
+const app = express();
+
+console.log("_dirname", __dirname);
+app.use(express.static(path.join(__dirname, "public")));
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+
+// 2- Sessions
+
+// 3-Views
+app.set('views',path.join(__dirname,"views"));
+app.set("views engine","ejs")
+
+
+// 4 Routers
+
+export default app;
