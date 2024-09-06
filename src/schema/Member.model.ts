55ed6a028a32871210db54c7cf95b3c1f7d745e6
Abrorkhon <akon.xan@mail.ru>
2024-09-06 18:27:27 +0900

    feat: Member scheme model

---
+++ src/schema/Member.model.ts
@@ -0,0 +1,51 @@
+import mongoose, { Schema } from "mongoose";
+import { MemberType, MemberStatus } from "../libs/enums/member.enum";
+
+/*two way to declare schemes 
+ => Scheme First & Code first
+*/
+const memberSchema = new Schema(
+  {
+    memberType: {
+      type: String,
+      enum: MemberType,
+      default: MemberType.USER,
+    },
+    memberStatus: {
+      type: String,
+      enum: MemberStatus,
+      default: MemberStatus.ACTIVE,
+    },
+    memberNick: {
+      type: String,
+      index: { unique: true, sparse: true },
+      required: true,
+    },
+    memberPhone: {
+      type: String,
+      indexe: { unique: true, sparse: true },
+      required: true,
+    },
+    memberPassword: {
+      type: String,
+      index: { unique: true, sparse: true },
+      select: false,
+      required: true,
+    },
+    memberAddress: {
+      type: String,
+    },
+    memberDesc: {
+      type: String,
+    },
+    memberImage: {
+      type: String,
+    },
+    memberPoints: {
+      type: String,
+      default: 0,
+    },
+  },
+  { timestamps: true }
+);
+export default mongoose.model("Memeber", memberSchema);
