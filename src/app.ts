1e9d121783c4f5309e126bd7820749843839f846
Abrorkhon <akon.xan@mail.ru>
2024-09-19 16:49:08 +0900

    feat: integrate middleware sessions

--- src/app.ts
+++ src/app.ts
@@ -4,6 +4,15 @@ import router from "./router";
 import routerAdmin from "./router-admin";
 import morgan from "morgan";
 import { MORGAN_FORMAT } from "./libs/config";
+import session from "express-session";
+import ConnectMongoDB from "connect-mongodb-session";
+
+const MongoDBStore = ConnectMongoDB(session);
+
+const store = new MongoDBStore({
+  uri: String(process.env.MONGO_URL),
+  collection: "sessions",
+});
 
 // 1-Entrance
 const app = express();
@@ -12,6 +21,18 @@ app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 app.use(morgan(MORGAN_FORMAT));
 // 2- Sessions
+
+app.use(
+  session({
+    secret: String(process.env.SESSION_SECRET),
+    cookie: {
+      maxAge: 1000 * 3600*3, // 3h
+    },
+    store: store,
+    resave: true,
+    saveUninitialized: true,
+  })
+);
 
 // 3-Views
 app.set("views", path.join(__dirname, "views"));
