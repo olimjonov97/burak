412d242a9d26fb0c709f9599de5aa060365fb09f
Abrorkhon <akon.xan@mail.ru>
2024-09-21 19:00:59 +0900

    feat:cinfig product scheme model

--- src/router-admin.ts
+++ src/router-admin.ts
@@ -29,7 +29,7 @@ routerAdmin.get(
 routerAdmin.post(
   "/product/create",
   restaurantController.verifyRestaurant,
-  makeUploader("products").array("productImages",5),
+  makeUploader("products").array("productImages",5 ),
   productController.createNewProduct
 );
 routerAdmin.post(
---
+++ src/schema/Product.model.ts
@@ -0,0 +1,68 @@
+import mongoose, { Schema } from "mongoose";
+import {
+  ProductCollection,
+  ProductSize,
+  ProductStatus,
+  ProductVolume,
+} from "../libs/enums/product.enum";
+
+/*two way to declare schemes 
+ => Scheme First & Code first
+*/
+/*
+frontend validaton
+backend validation
+database validation
+*/
+const productSchema = new Schema(
+  {
+    productStatus: {
+      type: String,
+      enum: ProductStatus,
+      default: ProductStatus.PAUSE,
+    },
+    productCollection: {
+      type: String,
+      enum: ProductCollection,
+      required: true,
+    },
+    productName: {
+      type: String,
+      required: true,
+    },
+    productPrice: {
+      type: Number,
+      required: true,
+    },
+    productLeftCount: {
+      type: Number,
+      required: true,
+    },
+    productSize: {
+      type: String,
+      enum: ProductSize,
+      default: ProductSize.NORMAL,
+    },
+    productVolume: {
+      type: String,
+      enum: ProductVolume,
+      default: ProductVolume.ONE,
+    },
+    productDesc: {
+      type: String,
+      required: true,
+    },
+    productImages: {
+      type: [String],
+      default: [],
+    },
+    productView: {
+      type: Number,
+      default: 0,
+    },
+  },
+  { timestamps: true }
+);
+
+productSchema.index({productName:1, productSize:1,productVolume:1},{unique:true})
+export default mongoose.model("Product", productSchema);
