4b9321fd04e60fb9e5c7d5b87cdfe3dafe992c9b
Abrorkhon <akon.xan@mail.ru>
2024-09-27 18:15:52 +0900

    Feat: signup process validation

--- src/public/js/signup.js
+++ src/public/js/signup.js
@@ -1,1 +1,25 @@
 console.log("Signup frontend javascript file");
+
+//FRONTEND VALIDATION
+
+$(function () {});
+
+function validationSignUpForm() {
+  const memberNick = $(".member-nick").val();
+  const memberPhone = $(".member-phone").val();
+  const memberPassword = $(".member-password").val();
+  const confirmPassword = $(".confirm-password").val();
+  if (
+    memberNick === "" ||
+    memberPhone === "" ||
+    memberPassword === "" ||
+    confirmPassword === ""
+  ) {
+    alert("Please Insert all requiured inputs");
+    return false;
+  }
+  if (memberPassword !== confirmPassword) {
+    alert(" Password Differs Please Check Your Password");
+    return false;
+  }
+}
