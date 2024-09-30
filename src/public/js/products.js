e57192612fe3f66edb3da7ca7cd42b35012814e9
Abrorkhon <akon.xan@mail.ru>
2024-10-01 08:54:24 +0900

    feat:update chosen product frontedn develop

--- src/public/js/products.js
+++ src/public/js/products.js
@@ -19,6 +19,25 @@ $(function () {
     $(".dish-container").slideToggle(100);
     $("#process-btn").css("display", "flex");
   });
+  $(".new-product-status").on("change", async function (e) {
+    const id = e.target.id;
+    const productStatus = $(`#${id}.new-product-status`).val();
+    console.log("id", id);
+    console.log("productStatus", productStatus);
+    try {
+      const response = await axios.post(`/admin/product/${id}`, {
+        productStatus: productStatus,
+      });
+      const result = response.data;
+      if (response.data) {
+        console.log("Producrt Updated");
+        $(".new-product-status").blur();
+      }else alert("Product Update failed")
+    } catch (err) {
+      console.log(err);
+      alert("Product Update failed");
+    }
+  });
 });
 
 function validateForm() {
