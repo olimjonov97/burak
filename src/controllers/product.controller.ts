2925f7a335eaaf3e300ad6f083214c52e938aec6
Abrorkhon <akon.xan@mail.ru>
2024-10-01 08:37:41 +0900

    feat:create new product frontend logic develop

--- src/controllers/product.controller.ts
+++ src/controllers/product.controller.ts
@@ -28,11 +28,11 @@ productController.createNewProduct = async (
   res: Response
 ) => {
   try {
-    console.log("createNewProduct");
-    // console.log("req.files)", req.files);
+    console.log("createNewProduct", req.body);
 
     if (!req.files?.length)
       throw new Errors(HttpCode.INTERNAL_SERVER_ERROR, Message.CREATE_FAILED);
+
     const data: ProductInput = req.body;
     data.productImages = req.files?.map((ele) => {
       return ele.path;
@@ -41,14 +41,14 @@ productController.createNewProduct = async (
     await productService.createNewProduct(data);
 
     res.send(
-      `<script> alert("Successfully Created !" );window.locaion.replace('admin/login') </script>`
+      `<script> alert("Successfully Created !" );window.location.replace('/admin/product/all') </script>`
     );
   } catch (err) {
     const message =
       err instanceof Errors ? err.message : Message.SOMETHING_WENT_WRONG;
     console.log(" Error On createNewProduct", err);
     res.send(
-      `<script> alert("${message}");window.locaion.replace('admin/login') </script>`
+      `<script> alert("${message}");window.location.replace('/admin/product/all') </script>`
     );
   }
 };
