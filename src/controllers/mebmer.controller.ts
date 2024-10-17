cb1a9e489d72925fd7f984d3fd335eb9a2743126
Abrorkhon <akon.xan@mail.ru>
2024-10-17 19:23:39 +0900

    feat create middle ware methods develop logout process

--- src/controllers/mebmer.controller.ts
+++ src/controllers/mebmer.controller.ts
@@ -1,8 +1,8 @@
 // REACT
-import { json, Request, Response } from "express";
+import { json, NextFunction, Request, Response } from "express";
 import { T } from "../libs/types/common";
 import MemberService from "../models/Member.service";
-import { LoginInput, Member, MemberInput } from "../libs/types/member";
+import { ExtendedRequest, LoginInput, Member, MemberInput } from "../libs/types/member";
 import Errors, { HttpCode, Message } from "../libs/Errors";
 import AuthService from "../models/Auth.service";
 import { AUTH_TIMER } from "../libs/config";
@@ -53,20 +53,50 @@ memberController.login = async (req: Request, res: Response) => {
     else res.status(Errors.standard.code).json(Errors.standard);
   }
 };
-memberController.verifyAuth = async (req: Request, res: Response) => {
+memberController.logout=(req:ExtendedRequest, res:Response)=>{
+try {
+  console.log("logout")
+  res.cookie("accessToken",null,{maxAge:0,httpOnly:true});
+  res.status(HttpCode.OK).json({logout:true})
+} catch (err) {
+  console.log(" Error On logout", err);
+  if (err instanceof Errors) res.status(err.code).json(err);
+  else res.status(Errors.standard.code).json(Errors.standard);
+}
+
+}
+memberController.verifyAuth = async (
+  req: ExtendedRequest,
+  res: Response,
+  next: NextFunction
+) => {
   try {
-    let member = null;
+
     const token = req.cookies["accessToken"]; //this is obj and we get the value by dynamic call
     console.log("cookies", req.cookies);
-    if (token) member = await authService.checkAuth(token);
-    if (!member)
+    if (token) req.member = await authService.checkAuth(token);
+    if (!req.member)
       throw new Errors(HttpCode.UNAUTHORIZED, Message.NOT_AUTHENTICATED);
-    console.log("member", member);
-    res.status(HttpCode.OK).json({ member: member });
+    console.log("member", req.member);
+   next();
   } catch (err) {
     console.log(" Error On verifyAuth", err);
     if (err instanceof Errors) res.status(err.code).json(err);
     else res.status(Errors.standard.code).json(Errors.standard);
+  }
+};
+memberController.retrieveAuth = async (
+  req: ExtendedRequest,
+  res: Response,
+  next: NextFunction
+) => {
+  try {
+    const token = req.cookies["accessToken"]; //this is obj and we get the value by dynamic call
+    if (token) req.member = await authService.checkAuth(token);
+    next();
+  } catch (err) {
+    console.log(" Error On retrieveAuth", err);
+    next();
   }
 };
 
